f52fd4bb25d707d104f166dd4d0e6cf3555d0478
Todor Komitov <todorkomitov@gmail.com>
2018-06-12 21:34:18 +0300

    Update App.java

--- demo/src/app/App.java
+++ demo/src/app/App.java
@@ -3,8 +3,11 @@ package app;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("Holla!");
-		int a = 5;
-		int b = 13;
+		//new comment here
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 }
