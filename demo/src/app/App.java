b2797e6748663b969fedc60c928b4e6ca982dda7
komitoff <todorkomitov@gmail.com>
2018-06-12 21:36:02 +0300

    message

--- demo/src/app/App.java
+++ demo/src/app/App.java
@@ -1,11 +1,9 @@
 package app;
 
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("Holla!");
-		int a = 5;
-		int b = 13;
-		System.out.println(a + b);
+		long b = 14;
+		System.out.println(b + 2L);
 	}
 }
